7dfd305206be8effa19d4cddfed08b9349c0e5f0
mbari60 <kevinmbari60@gmail.com>
2025-11-17 20:17:56 +0300

    Disable ESLint for production build

--- dan-pos/next.config.mjs
+++ dan-pos/next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
